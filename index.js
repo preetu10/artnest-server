c4a8b3675f8dbaf93c6dbf46109e664a34c7f770
Mahfuja <bajibaji1004@gmail.com>
2024-04-28 11:36:03 +0600

    2nd commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
@@ -9,6 +10,43 @@ app.use(cors());
 app.get('/', (req,res)=>{
     res.send("ArtNest running is running on server");
 });
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fxxuhv1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+   
+    const database=client.db("art-nest");
+    const crafts=database.collection("crafts");
+
+    app.get('/', (req,res)=>{
+        res.send("ArtNest running is running on server");
+    });
+    
+    app.post("/add-crafts",async(req, res)=>{
+        const item=req.body;
+        const result=await crafts.insertOne(item);
+        res.send(result);
+    })
+
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+
 app.listen(port,()=>{
     console.log("my app listening");
 })
